5c5b7b2ff57cd2d7fd7ec756c893dd40b5137d11
Yokubjanovichh <yokubjanovich@gmail.com>
2026-03-14 10:15:04 +0500

    added game page

---
+++ scripts/services-data.js
@@ -0,0 +1,187 @@
+// Shared service data used across pages.
+// This file defines global objects (no bundler/module system).
+
+window.NB_SERVICE_CONFIG = {
+  "gta-cash-cars": {
+    titleHtml: "GTA Online Cash+<br />Cars Boost PS4/PS5",
+    imageSrc: "../assets/images/services1.webp",
+    imageAlt: "GTA Online cash and cars boost",
+    options: [
+      "15 million - 19.99$",
+      "25 million - 23.99$",
+      "30 million - 27.99$",
+      "50 million - 33.99$",
+      "75 million - 39.99$",
+      "100 million - 49.99$",
+      "150 million - 60.99$",
+      "200 million - 70.99$",
+      "300 million - 85.99$",
+      "500 million - 120.99$",
+      "750 million - 150.99$",
+      "1 Billion - 199.99$",
+    ],
+    defaultOption: "15 million - 19.99$",
+  },
+  "gta-cash": {
+    titleHtml: "GTA Online Cash Boost<br />PS4/PS5",
+    imageSrc: "../assets/images/services2.webp",
+    imageAlt: "GTA Online cash boost",
+    options: [
+      "10 million - 19.99$",
+      "20 million - 24.99$",
+      "35 million - 32.99$",
+      "50 million - 39.99$",
+      "100 million - 59.99$",
+    ],
+    defaultOption: "10 million - 19.99$",
+  },
+  "gta-level": {
+    titleHtml: "GTA Online Level Boost<br />PS4/PS5",
+    imageSrc: "../assets/images/services3.webp",
+    imageAlt: "GTA Online level boost",
+    options: [
+      "Rank 50 - 29.99$",
+      "Rank 100 - 39.99$",
+      "Rank 150 - 49.99$",
+      "Rank 200 - 59.99$",
+      "Rank 300 - 79.99$",
+    ],
+    defaultOption: "Rank 50 - 29.99$",
+  },
+  "gta-modded": {
+    titleHtml: "GTA Online Modded Account<br />PS4/PS5",
+    imageSrc: "../assets/images/services4.webp",
+    imageAlt: "GTA Online modded account",
+    options: [
+      "Modded Basic - 29.99$",
+      "Modded Standard - 39.99$",
+      "Modded Pro - 49.99$",
+      "Modded Ultra - 69.99$",
+      "Modded Legend - 89.99$",
+    ],
+    defaultOption: "Modded Basic - 29.99$",
+  },
+};
+
+window.NB_GTA5_SERVICES = {
+  ps: [
+    {
+      serviceParam: "gta-cash-cars",
+      imageSrc: "../assets/images/services1.webp",
+      imageAlt: "GTA Online Cash + Cars Boost",
+      title: "GTA Online Cash + Cars Boost PS4/PS5",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-cash",
+      imageSrc: "../assets/images/services2.webp",
+      imageAlt: "GTA Online Cash Boost",
+      title: "GTA Online Cash Boost PS4/PS5",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-level",
+      imageSrc: "../assets/images/services3.webp",
+      imageAlt: "GTA Online Level Boost",
+      title: "GTA Online Level Boost PS4/PS5",
+      priceNow: "$29.99",
+    },
+    {
+      serviceParam: "gta-modded",
+      imageSrc: "../assets/images/services4.webp",
+      imageAlt: "GTA Online Modded Account",
+      title: "GTA Online Modded Account PS4/PS5",
+      priceNow: "$29.99",
+    },
+    {
+      serviceParam: "gta-cash-cars",
+      imageSrc: "../assets/images/services1.webp",
+      imageAlt: "GTA Online Cash + Cars Boost",
+      title: "GTA Online Cash + Cars Boost PS4/PS5",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-cash-cars",
+      imageSrc: "../assets/images/services2.webp",
+      imageAlt: "GTA Online Cash + Cars Boost",
+      title: "GTA Online Cash + Cars Boost PS4/PS5",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-cash-cars",
+      imageSrc: "../assets/images/services3.webp",
+      imageAlt: "GTA Online Cash + Cars Boost",
+      title: "GTA Online Cash + Cars Boost PS4/PS5",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-cash-cars",
+      imageSrc: "../assets/images/services4.webp",
+      imageAlt: "GTA Online Cash + Cars Boost",
+      title: "GTA Online Cash + Cars Boost PS4/PS5",
+      priceNow: "$19.99",
+    },
+  ],
+
+  pc: [
+    {
+      serviceParam: "gta-cash-cars",
+      imageSrc: "../assets/images/services1.webp",
+      imageAlt: "GTA Online Cash + Cars Boost",
+      title: "GTA Online Cash + Cars Boost PC",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-cash",
+      imageSrc: "../assets/images/services2.webp",
+      imageAlt: "GTA Online Cash Boost",
+      title: "GTA Online Cash Boost PC",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-level",
+      imageSrc: "../assets/images/services3.webp",
+      imageAlt: "GTA Online Level Boost",
+      title: "GTA Online Level Boost PC",
+      priceNow: "$29.99",
+    },
+    {
+      serviceParam: "gta-modded",
+      imageSrc: "../assets/images/services4.webp",
+      imageAlt: "GTA Online Modded Account",
+      title: "GTA Online Modded Account PC",
+      priceNow: "$29.99",
+    },
+  ],
+
+  xbox: [
+    {
+      serviceParam: "gta-cash-cars",
+      imageSrc: "../assets/images/services1.webp",
+      imageAlt: "GTA Online Cash + Cars Boost",
+      title: "GTA Online Cash + Cars Boost Xbox One/Series",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-cash",
+      imageSrc: "../assets/images/services2.webp",
+      imageAlt: "GTA Online Cash Boost",
+      title: "GTA Online Cash Boost Xbox One/Series",
+      priceNow: "$19.99",
+    },
+    {
+      serviceParam: "gta-level",
+      imageSrc: "../assets/images/services3.webp",
+      imageAlt: "GTA Online Level Boost",
+      title: "GTA Online Level Boost Xbox One/Series",
+      priceNow: "$29.99",
+    },
+    {
+      serviceParam: "gta-modded",
+      imageSrc: "../assets/images/services4.webp",
+      imageAlt: "GTA Online Modded Account",
+      title: "GTA Online Modded Account Xbox One/Series",
+      priceNow: "$29.99",
+    },
+  ],
+};
